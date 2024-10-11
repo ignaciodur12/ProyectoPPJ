45630ed6b456930ff73ed3106f626572171e4155
ignaciodur12 <ignaciosanhueza1516@gmail.com>
2024-10-11 14:33:14 -0300

    first commit

---
+++ src/main/java/Proyectoppj/ApWebGestionApplication.java
@@ -0,0 +1,13 @@
+package Proyectoppj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApWebGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApWebGestionApplication.class, args);
+	}
+
+}
